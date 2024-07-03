a9792684007faa9fde350a78ec3cc532c0aee3d5
zizfiq <fiqri.aaziz@gmail.com>
2024-07-03 21:37:21 +0700

    chg name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kabupaten/Kota App',
+      title: 'Tracking Cuaca App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: false,
